bf29cd3826bfbe37900c3a576f2b96001c56d911
Felipe Lopes <felipelm07@gmail.com>
2020-10-16 00:33:23 -0300

    ajuste

--- imprimir.php
+++ imprimir.php
@@ -1,11 +1,31 @@
 <?php
+
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 header("Access-Control-Allow-Methods: GET, POST");
 header('Content-type: application/json');
-    
-$pedido = $_POST["pedido"];
 
-echo json_encode(true);
+require 'class/vendor/autoload.php';
+
+use Mike42\Escpos\Printer;
+use Mike42\Escpos\EscposImage;
+use Mike42\Escpos\PrintConnectors\WindowsPrintConnector;
+
+//$pedido = $_POST["pedido"];
+$nomeimpress = "ELGINi9";
+
+try {
+
+    $connector = new WindowsPrintConnector($nomeimpress);
+    $printer = new Printer($connector);
+    $printer->text("Pedido\n");
+    $printer->cut();
+    $printer->close();
+    
+    echo json_encode(true);
+} catch (Exception $exc) {
+    echo $exc->getTraceAsString();
+    //echo json_encode(false);
+}
 
 ?>
